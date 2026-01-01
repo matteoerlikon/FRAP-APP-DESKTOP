942a4ea86c780efa25c06c347e8dcda51b659325
matteoerlikon <matteo.sardioerlikon@gmail.com>
2026-01-01 16:05:33 +0100

    NEW PWA

--- sw.js
+++ sw.js
@@ -1,5 +1,4 @@
-const CACHE_NAME = 'frap-cache-v4';
+const CACHE_NAME = 'frap-v5';
 const ASSETS = ['./', './index_pwa.html', './LOGO_FRAP.PNG', './manifest.json'];
 self.addEventListener('install', e => { e.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(ASSETS))); });
-self.addEventListener('activate', e => { e.waitUntil(caches.keys().then(keys => Promise.all(keys.map(k => { if(k !== CACHE_NAME) return caches.delete(k); })))); });
 self.addEventListener('fetch', e => { e.respondWith(caches.match(e.request).then(r => r || fetch(e.request))); });
